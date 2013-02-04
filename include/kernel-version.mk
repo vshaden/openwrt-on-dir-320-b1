a3e4cc819da7022fe28b67d647fe72e1d80c0c5d
xakep6666 <xakep6666@gmail.com>
2013-02-04 14:43:17 +0000

    Inital commit. Using OpenWRT Barrier Breaker r35482

---
+++ include/kernel-version.mk
@@ -0,0 +1,27 @@
+# Use the default kernel version if the Makefile doesn't override it
+
+LINUX_RELEASE?=1
+
+ifeq ($(LINUX_VERSION),3.3.8)
+  LINUX_KERNEL_MD5SUM:=f1058f64eed085deb44f10cee8541d50
+endif
+ifeq ($(LINUX_VERSION),3.6.11)
+  LINUX_KERNEL_MD5SUM:=3d602ad7f7984509c3e923a5ae90bc54
+endif
+ifeq ($(LINUX_VERSION),3.7.6)
+  LINUX_KERNEL_MD5SUM:=4c4d80619c713f83015fc6b1c6280891
+endif
+
+# disable the md5sum check for unknown kernel versions
+LINUX_KERNEL_MD5SUM?=x
+
+split_version=$(subst ., ,$(1))
+merge_version=$(subst $(space),.,$(1))
+KERNEL_BASE=$(firstword $(subst -, ,$(LINUX_VERSION)))
+KERNEL=$(call merge_version,$(wordlist 1,2,$(call split_version,$(KERNEL_BASE))))
+ifeq ($(firstword $(call split_version,$(KERNEL_BASE))),2)
+  KERNEL_PATCHVER=$(call merge_version,$(wordlist 1,3,$(call split_version,$(KERNEL_BASE))))
+else
+  KERNEL_PATCHVER=$(KERNEL)
+endif
+
