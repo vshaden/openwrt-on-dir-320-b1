80ef01a623b1b7a47ff131b86515dc87f3184202
xakep6666@gmail.com <xakep6666@gmail.com@d76aff79-9611-8cfc-d104-c7f6b407677d>
2013-02-17 13:00:50 +0000

    Update to OpenWRT Trunk r35635

--- trunk/include/kernel-version.mk
+++ trunk/include/kernel-version.mk
@@ -8,8 +8,8 @@ endif
 ifeq ($(LINUX_VERSION),3.6.11)
   LINUX_KERNEL_MD5SUM:=3d602ad7f7984509c3e923a5ae90bc54
 endif
-ifeq ($(LINUX_VERSION),3.7.6)
-  LINUX_KERNEL_MD5SUM:=4c4d80619c713f83015fc6b1c6280891
+ifeq ($(LINUX_VERSION),3.7.8)
+  LINUX_KERNEL_MD5SUM:=4bc40d18bf90f25d5463cfb9442876af
 endif
 
 # disable the md5sum check for unknown kernel versions
