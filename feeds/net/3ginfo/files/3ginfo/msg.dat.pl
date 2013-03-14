54c9293078c0b57b953d739d4ffb32aa69697995
rzrusink@gmail.com <rzrusink@gmail.com@d76aff79-9611-8cfc-d104-c7f6b407677d>
2013-03-14 21:52:53 +0000

    Deleted

--- feeds/net/3ginfo/files/3ginfo/msg.dat.pl
+++ feeds/net/3ginfo/files/3ginfo/msg.dat.pl
@@ -1,8 +1,1 @@
-NOTDETECTED="Nie wykryto żadnego urządzenia!"
-NODEVICE="Brak urządzenia"
-PINERROR="Błąd kodu PIN"
-CONNECTED="Połączony"
-CONNECT="Połącz"
-DISCONNECTED="Rozłączony"
-DISCONNECT="Rozłącz"
-NOINFO="Brak informacji"
+
