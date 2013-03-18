63a7377011c163193cdb60550cd7221a74fc23ec
xakep6666@gmail.com <xakep6666@gmail.com@d76aff79-9611-8cfc-d104-c7f6b407677d>
2013-03-18 16:25:13 +0000

    Fixed luci-3ginfo

--- luci/trunk/applications/luci-3ginfo/luasrc/model/cbi/3ginfo.lua
+++ luci/trunk/applications/luci-3ginfo/luasrc/model/cbi/3ginfo.lua
@@ -14,35 +14,36 @@ You may obtain a copy of the License at
 
 local uci = luci.model.uci.cursor_state()
 local net = require "luci.model.network"
-local m, dev, pin, port, qos, lang
+local m
+local s
+local dev, pin, port, qos, lang
 local try_devices = nixio.fs.glob("/dev/ttyUSB*") or nixio.fs.glob("/dev/ttyACM*") or nixio.fs.glob("/dev/cdc*")
 
-m = Map("3ginfo", translate("3ginfo - information about 3g/4g connection"),
+m = Map("3ginfo", translate("3ginfo"),
         translatef("First you have to install the packages to get support for 3g/4g modems."))
 
 net = net.init(m.uci)
+s = m:section(TypedSection, "3ginfo", translate("Settings"))
 
-dev = m:section(Value, "device", translate("Device"))
-dev:value("", "")
+dev = s:option(Value, "device", translate("Device"))
 if try_devices then
 	local node
 	for node in try_devices do
-		dev:value(node)
+		dev:value(node, node)
 	end
 end
-dev.default = ""
 
-pin = m:section(Value, "pincode", translate("PIN code (only for 3g), optional"))
+pin = s:option(Value, "pincode", translate("PIN code (only for 3g), optional"))
 pin.default = ""
 
-port = m:section(Value, "http_port", translate("Port for HTTP connections"))
+port = s:option(Value, "http_port", translate("Port for HTTP connections"))
 port.default = "81"
 
-qos = m:section(Flag, "qos", translate("Enable QOS support"))
+qos = s:option(Flag, "qos", translate("Enable QOS support"))
 qos.enabled = "1"
 qos.disabled = "0"
 
-lang = m:section(Value, "language", translate("Language"))
+lang = s:option(Value, "language", translate("Language"))
 lang:value("ru", "Russian")
 lang:value("en", "English")
 lang.default = "ru"
