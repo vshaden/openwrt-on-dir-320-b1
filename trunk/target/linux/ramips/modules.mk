56dc720ebabe6ce91644230d09343ae966a45316
xakep6666@gmail.com <xakep6666@gmail.com@d76aff79-9611-8cfc-d104-c7f6b407677d>
2013-07-25 17:37:33 +0000

    Move to 3.10 kernel + RT5350 temperature compensation fix

--- trunk/target/linux/ramips/modules.mk
+++ trunk/target/linux/ramips/modules.mk
@@ -5,22 +5,17 @@
 # See /LICENSE for more information.
 #
 
-define KernelPackage/usb-rt305x-dwc_otg
-  TITLE:=RT305X USB controller driver
-  DEPENDS:=@TARGET_ramips_rt305x
-  KCONFIG:= \
-	CONFIG_DWC_OTG \
-	CONFIG_DWC_OTG_HOST_ONLY=y \
-	CONFIG_DWC_OTG_DEVICE_ONLY=n \
-	CONFIG_DWC_OTG_DEBUG=n
-  FILES:=$(LINUX_DIR)/drivers/usb/dwc_otg/dwc_otg.ko
-  AUTOLOAD:=$(call AutoLoad,54,dwc_otg,1)
-  $(call AddDepends/usb)
+I2C_RALINK_MODULES:= \
+  CONFIG_I2C_RALINK:drivers/i2c/busses/i2c-ralink
+
+define KernelPackage/i2c-ralink
+  $(call i2c_defaults,$(I2C_RALINK_MODULES),59)
+  TITLE:=Ralink I2C Controller
+  DEPENDS:=@TARGET_ramips kmod-i2c-core
 endef
 
-define KernelPackage/usb-rt305x-dwc_otg/description
-  This driver provides USB Device Controller support for the
-  Synopsys DesignWare USB OTG Core used in the Ralink RT305X SoCs.
+define KernelPackage/i2c-ralink/description
+ Kernel modules for enable ralink i2c controller.
 endef
 
-$(eval $(call KernelPackage,usb-rt305x-dwc_otg))
+$(eval $(call KernelPackage,i2c-ralink))
--- trunk/target/linux/ramips/rt305x/profiles/dlink.mk
+++ trunk/target/linux/ramips/rt305x/profiles/dlink.mk
@@ -8,7 +8,7 @@
 define Profile/DIR615H1
 	NAME:=Dlink DIR-615 H1
         PACKAGES:=\
-                -kmod-usb-core -kmod-usb-rt305x-dwc_otg \
+                -kmod-usb-core -kmod-usb-dwc2 \
                 -kmod-ledtrig-usbdev
 endef
 
@@ -18,16 +18,3 @@ endef
 
 $(eval $(call Profile,DIR615H1))
 
-define Profile/DIR-320-B1
-	NAME:=D-Link DIR-320/NRU
-	PACKAGES:=\
-		kmod-usb-core kmod-usb-rt305x-dwc_otg \
-		kmod-ledtrig-usbdev kmod-mppe kmod-rt2800-pci \
-		kmod-usb2 kmod-usb-ohci kmod-ledtrig-netdev xxd sed
-endef
-
-define Profile/DIR-320-B1/Description
-	Package set for DIR-320/NRU board
-endef
-
-$(eval $(call Profile,DIR-320-B1))
