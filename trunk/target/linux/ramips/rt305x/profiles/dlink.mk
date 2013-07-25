ae4fe435525d5e5f550c77bf8fed4d34989787b8
xakep6666@gmail.com <xakep6666@gmail.com@d76aff79-9611-8cfc-d104-c7f6b407677d>
2013-07-25 17:41:54 +0000

    Move to 3.10 kernel + RT5350 temperature compensation fix

--- trunk/target/linux/ramips/rt305x/profiles/dlink.mk
+++ trunk/target/linux/ramips/rt305x/profiles/dlink.mk
@@ -18,3 +18,16 @@ endef
 
 $(eval $(call Profile,DIR615H1))
 
+define Profile/DIR-320-B1
+	NAME:=D-Link DIR-320/NRU
+	PACKAGES:=\
+		kmod-usb-core kmod-usb-rt305x-dwc_otg \
+		kmod-ledtrig-usbdev kmod-mppe kmod-rt2800-pci \
+		kmod-usb2 kmod-usb-ohci kmod-ledtrig-netdev xxd sed
+endef
+
+define Profile/DIR-320-B1/Description
+	Package set for DIR-320/NRU board
+endef
+
+$(eval $(call Profile,DIR-320-B1))
